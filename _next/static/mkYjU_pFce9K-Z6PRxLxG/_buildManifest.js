ada57ac7cee9bc545a001f93fb176be66e245ec9
Krina Parmar <krinaparmar2985@gmail.com>
2026-06-14 23:21:03 +0530

    Updates

--- _next/static/mkYjU_pFce9K-Z6PRxLxG/_buildManifest.js
+++ _next/static/mkYjU_pFce9K-Z6PRxLxG/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/worksphere//_next/:path+",
+        "destination": "/worksphere/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
